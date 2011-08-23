6f202c7278121a66303a1b08c77771cebf291849
Glen.R.White@gmail.com <Glen.R.White@gmail.com@b07e7a40-ffb5-134e-ba8c-5bacba8045f1>
2011-08-23 00:23:19 +0000

    add release statement to build file

--- trunk/src/mexsrc/matgm/build.m
+++ trunk/src/mexsrc/matgm/build.m
@@ -5,3 +5,4 @@ mex -c xsif_size_pars.f xsif_elem_pars.f
 mex -c lattice_mod.F gm_model_mod.f
 mex -c gm_model.f
 mex matgm_mex.F gm_model.o gm_model_mod.o control_mod.o lattice_mod.o
+!mv matgm_mex.mex* ../../GroundMotion/
