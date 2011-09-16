093c032efbb03a75461a97525aa0235ed9bdf347
glen.r.white <Glen.R.White@gmail.com>
2011-09-16 15:49:28 +0000

    development

--- src/LatticeGeneration/RenormalizePS.m
+++ src/LatticeGeneration/RenormalizePS.m
@@ -1,4 +1,4 @@
-function stat = RenormalizePS( klysno )
+function stat = RenormalizePS( psno )
 
 % RENORMALIZEPS Rescale a power supply such that its amplitude == 1.0
 %
@@ -10,6 +10,10 @@ function stat = RenormalizePS( klysno )
 %       stat{2...} are text error messages.
 
 % MOD:
+%        7-Sept-2011, GRW:
+%           changed klyno->psno and added temp fix for bug where
+%           BEAMLINE.PS list is correct but PS.Element list doesn't match
+%           when BEAMLINE elements with same PS but different names
 %        29-sep-2005, PT:
 %           support for magnets with multiple power supplies.
 %        28-sep-2005, PT:
@@ -17,51 +21,78 @@ function stat = RenormalizePS( klysno )
 
 %==========================================================================
 
-  global BEAMLINE ;
-  global PS ;
-  stat = InitializeMessageStack( ) ;
-  
+global BEAMLINE ;
+global PS ;
+stat = InitializeMessageStack( ) ;
+
 % Is the desired PS in range?
 
-  if ( klysno > length(PS) )
-      stat = AddMessageToStack(stat,...
-          ['PS # ',num2str(klysno),...
-          ' out of range in MovePhysicsVarsToPS']) ;
-      stat{1} = 0 ;
-  end
-  
+if ( psno > length(PS) )
+  stat = AddMessageToStack(stat,...
+    ['PS # ',num2str(psno),...
+    ' out of range in MovePhysicsVarsToPS']) ;
+  stat{1} = 0 ;
+end
+
 % if the PS amplitude is zero, just go to its magnets and set their B
 % values to 1
+% (apply to all elements in same block with same PS)
 
-  if (PS(klysno).Ampl == 0)
-     for elemno = PS(klysno).Element
-       if (length(BEAMLINE{elemno}.PS) == 1)
-         BEAMLINE{elemno}.B = ones(1,length(BEAMLINE{elemno}.B)) ;
-       else   
-         PS_index = find(BEAMLINE{elemno}.PS == klysno) ;
-         BEAMLINE{elemno}.B(PS_index) = 1 ;
-       end    
-     end
-     return ;
+if (PS(psno).Ampl == 0)
+  for elemno = PS(psno).Element
+    if ~elemno; continue; end;
+    if isfield(BEAMLINE{elemno},'Block') && ~isempty(BEAMLINE{elemno}.Block)
+      for ibl=BEAMLINE{elemno}.Block(1):BEAMLINE{elemno}.Block(end)
+        if isfield(BEAMLINE{ibl},'PS') && any(BEAMLINE{ibl}.PS==psno)
+          if (length(BEAMLINE{ibl}.PS) == 1)
+            BEAMLINE{ibl}.B = ones(1,length(BEAMLINE{ibl}.B)) ;
+          else
+            BEAMLINE{ibl}.B(BEAMLINE{ibl}.PS == psno) = 1 ;
+          end
+        end
+      end
+    else
+      if (length(BEAMLINE{elemno}.PS) == 1)
+        BEAMLINE{elemno}.B = ones(1,length(BEAMLINE{elemno}.B)) ;
+      else
+        BEAMLINE{elemno}.B(BEAMLINE{elemno}.PS == psno) = 1 ;
+      end
+    end
   end
-  
+  return ;
+end
+
 % compute the scale factor
 
-  scale = 1 / PS(klysno).Ampl ;
-  
+scale = 1 / PS(psno).Ampl ;
+
 % apply the scale factor to the PS
 
-  PS(klysno).Ampl = PS(klysno).Ampl * scale ;
-  PS(klysno).Step = PS(klysno).Step * scale ;
-  PS(klysno).SetPt = PS(klysno).SetPt * scale ;
-  
-% now apply the reverse transformation on elements 
+PS(psno).Ampl = PS(psno).Ampl * scale ;
+PS(psno).Step = PS(psno).Step * scale ;
+PS(psno).SetPt = PS(psno).SetPt * scale ;
 
-  for elemno = PS(klysno).Element
-       if (length(BEAMLINE{elemno}.PS) == 1) 
-         BEAMLINE{elemno}.B = BEAMLINE{elemno}.B / scale ;
-       else
-         PS_index = find(BEAMLINE{elemno}.PS == klysno) ;  
-         BEAMLINE{elemno}.B(PS_index) = BEAMLINE{elemno}.B(PS_index) / scale ;
-       end    
+% now apply the reverse transformation on elements
+% (apply to all elements in same block with same PS)
+doneibl=[];
+for elemno = PS(psno).Element
+  if ~elemno; continue; end;
+  if isfield(BEAMLINE{elemno},'Block') && ~isempty(BEAMLINE{elemno}.Block)
+    for ibl=BEAMLINE{elemno}.Block(1):BEAMLINE{elemno}.Block(end)
+      if isfield(BEAMLINE{ibl},'PS') && any(BEAMLINE{ibl}.PS==psno) && ~ismember(ibl,doneibl)
+        if (length(BEAMLINE{elemno}.PS) == 1)
+          BEAMLINE{ibl}.B = BEAMLINE{ibl}.B ./ scale ;
+        else
+          BEAMLINE{ibl}.B(BEAMLINE{ibl}.PS == psno) = BEAMLINE{ibl}.B(BEAMLINE{ibl}.PS == psno) ./ scale ;
+        end
+        doneibl(end+1)=ibl;
+      end
+    end
+  else
+    if (length(BEAMLINE{elemno}.PS) == 1)
+      BEAMLINE{elemno}.B = BEAMLINE{elemno}.B ./ scale ;
+    else
+      BEAMLINE{elemno}.B(BEAMLINE{elemno}.PS == psno) = BEAMLINE{elemno}.B(BEAMLINE{elemno}.PS == psno) ./ scale ;
+    end
   end
+end
