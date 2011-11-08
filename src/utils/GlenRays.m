2ab26fe276c747524d0b63cc5adde40a8bfda6e5
glen.r.white <Glen.R.White@gmail.com>
2011-11-08 10:23:39 +0000

    cleanup of utils

--- src/utils/GlenRays.m
+++
@@ -1,8 +0,0 @@
-% load 'beam.mat'
-rays0=[beamout.Bunch.x]';
-P=mean(rays0(:,6)); % GeV/c
-dp=(rays0(:,6)-P)/P;
-rays0(:,6)=dp;
-
-[rays,xv,sigm,sigv,emit,emitn,twss,eta1,eta2]=AnalyzeRays(rays0,P,3,1);
-PlotRays(rays,'Lucretia',0)
