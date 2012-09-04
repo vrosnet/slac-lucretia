a2434028a21ef755583e1f0b0d8d7492f8e66a5a
Glen.R.White@gmail.com <Glen.R.White@gmail.com@b07e7a40-ffb5-134e-ba8c-5bacba8045f1>
2012-09-04 18:13:56 +0000

    add SigmaFit capability to Match routine

--- trunk/src/TuningAndAnalysis/beamTerms.m
+++ trunk/src/TuningAndAnalysis/beamTerms.m
@@ -1,5 +1,5 @@
-function [fitTerm,fitCoef,bsize_corrected,bsize,p] = beamTerms(dim,beam,doGaussfit)
-% [fitTerm,fitCoef,bsize_corrected,bsize] = beamTerms(dim,beam [,doGaussfit])
+function [fitTerm,fitCoef,bsize_corrected,bsize,p] = beamTerms(dim,beam)
+% [fitTerm,fitCoef,bsize_corrected,bsize] = beamTerms(dim,beam)
 % Show relative contributions of up to 3rd-order beam correlations to the
 % beam size in the requested dimension
 % 3rd order polynomial fit to 4 independent variables vs specified (dim)
@@ -9,27 +9,22 @@ function [fitTerm,fitCoef,bsize_corrected,bsize,p] = beamTerms(dim,beam,doGaussf
 % -------
 % dim = required lucretia beam dimension to correlate
 % beam = lucretia beam
-% doGaussfit (optional): if provided and true then bsize returned parameter
-% shows effect on gaussian fit to core of distribution, else it is just the
-% RMS
 %
 % OUTPUTS:
 % --------
-% fitTerm(35,6) = Beam correlation (e.g. if dim=3 [1 0 0 0 0 0] = SIGMA(3,1)
+% fitTerm(56,6) = Beam correlation (e.g. if dim=3 [1 0 0 0 0 0] = SIGMA(3,1)
 %                                                 [1 0 0 1 0 0] = T314
 %                                                 [2 1 0 0 0 0] = U3112 etc... )
-% fitCoef(1,35) = coefficient related to corresponding beam correlation
+% fitCoef(1,56) = coefficient related to corresponding beam correlation
 % bsize_corrected(1,3) = RMS beam size in dimension dim when all 1st-3rd order
 %                        correlations are subtracted
-% bsize(1,35) = RMS beam size improvement due to corresponding correlation term
+% bsize(1,56) = RMS beam size improvement due to corresponding correlation term
 %               being subtracted from beam
 % p = results structure from polyfitn
 
 % Warnings not caring about here
 warnstate=warning('query','MATLAB:nearlySingularMatrix');
 warning('off','MATLAB:nearlySingularMatrix');
-
-if ~exist('doGaussfit','var'); doGaussfit=false; end;
 
 % remove constant offsets
 for idim=1:6
@@ -43,36 +38,18 @@ yfit=beam.Bunch.x(dim,:)';
 bsize_corrected=zeros(1,3);
 for iorder=1:3
   p=polyfitn(xfit,yfit,iorder);
-  if doGaussfit
-    nbin=max([length(yfit)/100 100]);
-    [ fx , bc ] = hist(yfit-polyvaln(p,xfit),nbin) ;
-    [~, q] = gauss_fit(bc,fx) ;
-    bsize_corrected(iorder) = q(4) ;
-  else
-    bsize_corrected(iorder)=std(yfit-polyvaln(p,xfit));
-  end
+  bsize_corrected(iorder)=std(yfit-polyvaln(p,xfit));
 end
-xfit=beam.Bunch.x';
 p=polyfitn(xfit,yfit,iorder);
 % Get contribution of beamsize to each fit term
-[Y I]=sort(sum(p.ModelTerms,2));
+[~, I]=sort(sum(p.ModelTerms,2));
 bsize=zeros(length(p.Coefficients),1);
-fitTerm=zeros(length(p.Coefficients),length(p.ModelTerms(1,:)));
+fitTerm=zeros(length(p.Coefficients),6);
 fitCoef=zeros(1,length(p.Coefficients));
 for iterm=1:length(p.Coefficients)
   p2=polyfitn(xfit,yfit,p.ModelTerms(I(iterm),:));
-  if Y(iterm)>0 && ~(length(find(p.ModelTerms(I(iterm),:)))==1 && find(p.ModelTerms(I(iterm),:))==dim)
-    yfit=yfit-polyvaln(p2,xfit);
-    if doGaussfit
-      nbin=max([length(yfit)/100 100]);
-      [ fx , bc ] = hist(polyvaln(p2,xfit),nbin);
-      [~, q] = gauss_fit(bc,fx) ;
-      bsize(iterm)=q(4);
-    else
-      bsize(iterm)=std(polyvaln(p2,xfit));
-    end
-  end
-  fitTerm(iterm,:)=p.ModelTerms(I(iterm),:);
+  bsize(iterm)=std(yfit)-std(yfit-polyvaln(p2,xfit));
+  fitTerm(iterm,~ismember(allpar,dim))=p.ModelTerms(I(iterm),:);
   fitCoef(iterm)=p2.Coefficients;
 end
 
