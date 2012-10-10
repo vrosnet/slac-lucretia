e2aa11df487c6f637064c735bdbdc347f8b1fffb
glen.r.white <Glen.R.White@gmail.com>
2012-10-10 20:41:31 +0000

    beam image plotting function

---
+++ src/TuningAndAnalysis/beamImage.m
@@ -0,0 +1,122 @@
+function [sigx,sigy,rmsx,rmsy]=beamImage(beam,nsig,E0,alim)
+% [sigx,sigy,rmsx,rmsy]=beamImage(beam,nsig [,E0,alim])
+%
+% Graphical plot of Lucretia beam in transverse plane including gaussian fits
+% beam = Lucretia beam structure
+% nsig = cut on transverse dimension
+% E0 (optional) = centroid beam energy
+% alim (optional) = force axis limits
+%
+% ===========================================
+% MDW, Oct 10 2012
+
+id=find(beam.Bunch.stop==0);
+rays=beam.Bunch.x(:,id)';
+
+name={'X (um)','PX (ur)','Y (um)','PY (ur)','Z (um)','dP (%)'};
+conv=[1e6,1e6,1e6,1e6,1e6,1e2];
+nbin=100;
+asym=false;
+npix=1024;
+
+x=conv(1)*rays(:,1);
+px=conv(2)*rays(:,2);
+y=conv(3)*rays(:,3);
+py=conv(4)*rays(:,4);
+z=conv(5)*rays(:,5);
+E=rays(:,6);
+if (~exist('E0','var')),E0=mean(E);end
+dp=conv(6)*(E-E0)/E0;
+
+if (nsig>0)
+  v={'x','px','y','py','z','dp'};
+  for n=1:5 % don't do dp (FACET fully compressed)
+    eval(['u=',v{n},';'])
+    N=length(u);
+    nit=0;
+    while 1
+      u0=mean(u);
+      sig=std(u);
+      id=find(abs(u-u0)<nsig*sig);
+      u=u(id);
+      if (length(u)==N),break,end
+      N=length(u);
+      nit=nit+1;
+    end
+    eval([v{n},'=u(id);'])
+    eval(['id',v{n},'=id;'])
+  end
+else
+  idx=(1:length(x))';
+  idpx=(1:length(x))';
+  idy=(1:length(x))';
+  idpy=(1:length(x))';
+  idz=(1:length(x))';
+  iddp=(1:length(x))';
+end
+
+subplot(221)
+[v,u]=hist(y,nbin);
+warning off MATLAB:rankDeficientMatrix
+warning off MATLAB:nearlySingularMatrix
+if (asym)
+  [yfit,q,dq,chi2]=agauss_fit(u,v,[],0);
+else
+  [yfit,q,dq,chi2]=gauss_fit(u,v,[],0);
+end
+warning on MATLAB:rankDeficientMatrix
+warning on MATLAB:nearlySingularMatrix
+sigy=q(4);
+h1=barh(u,v);
+ylim=get(gca,'YLim');
+set(h1,'EdgeColor',[0,0,1],'FaceColor',[0,0,1])
+hold on
+plot(yfit,u,'r-')
+hold off
+title(sprintf('\\sigma_y = %.1f um',sigy))
+ylabel('y (um)')
+
+subplot(224)
+[v,u]=hist(x,nbin);
+warning off MATLAB:rankDeficientMatrix
+warning off MATLAB:nearlySingularMatrix
+if (asym)
+  [xfit,q,dq,chi2]=agauss_fit(u,v,[],0);
+else
+  [xfit,q,dq,chi2]=gauss_fit(u,v,[],0);
+end
+warning on MATLAB:rankDeficientMatrix
+warning on MATLAB:nearlySingularMatrix
+sigx=q(4);
+h2=bar(u,v);
+xlim=get(gca,'XLim');
+set(h2,'EdgeColor',[0,0,1],'FaceColor',[0,0,1])
+hold on
+plot(u,xfit,'r-')
+hold off
+title(sprintf('\\sigma_x = %.1f um',sigx))
+xlabel('x (um)')
+
+ulim=max(abs([xlim,ylim]));
+if (~exist('alim','var'))
+  alim=ulim;
+end
+subplot(221),set(gca,'YLim',[-alim,alim]),axis square
+subplot(224),set(gca,'XLim',[-alim,alim]),axis square
+
+subplot(222)
+id=intersect(idx,idy);
+rmsx=std(x(id));
+rmsy=std(y(id));
+beamprof=hist2(x(id),y(id), ...
+  linspace(-ulim,ulim,npix),linspace(-ulim,ulim,npix));
+imagesc([-ulim,ulim],[-ulim,ulim],beamprof)
+set(gca,'YDir','normal')
+axis square
+axis([-alim,alim,-alim,alim])
+title(sprintf('%d\\sigma cuts: rmsx= %.1f um, rmsy= %.1f um', ...
+  nsig,rmsx,rmsy))
+ylabel('y (um)')
+xlabel('x (um)')
+
+end
