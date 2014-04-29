a9ab318a1b6150c86e4e49bf12feb84461ca67b0
Glen.R.White@gmail.com <Glen.R.White@gmail.com@b07e7a40-ffb5-134e-ba8c-5bacba8045f1>
2014-04-29 17:29:08 +0000

    LSC and G4 updates

--- trunk/src/mexsrc/packageData.sh
+++ trunk/src/mexsrc/packageData.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-printenv | grep G4 | grep DATA | sed 's/G4\w*=//' | xargs -I@ cp -r @ /var/MATLAB/G4Data/
+printenv | grep G4 | grep DATA | sed 's/G4\w*=//' | xargs -I@ cp -r @ ~/Lucretia/src/ExtProcess/G4Data/
