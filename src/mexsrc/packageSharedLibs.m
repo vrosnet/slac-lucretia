2a68dc1a3a5a68e94d3a800f9dcf205fce925b4b
glen.r.white <glen.r.white@b07e7a40-ffb5-134e-ba8c-5bacba8045f1>
2015-03-13 05:29:23 +0000

    mac 2015a builds

--- src/mexsrc/packageSharedLibs.m
+++ src/mexsrc/packageSharedLibs.m
@@ -1,11 +1,20 @@
 % Make list of all shared object libs for compiled mex binaries and copy
 % into Lucretia Libs directory
 exclude={'libstdc++'};
-[~,solibs]=system(sprintf('find ../* -name "*.%s" | xargs ldd | grep "=> /"',mexext));
+if ismac
+  [~,solibs1]=system(sprintf('find ../* -name "*.%s" | xargs otool -L | grep "/Users"',mexext));
+  [~,solibs2]=system(sprintf('find ../* -name "*.%s" | xargs otool -L | grep "/usr/local"',mexext));
+  solibs=[solibs1 solibs2];
+else
+  [~,solibs]=system(sprintf('find ../* -name "*.%s" | xargs ldd | grep "=> /"',mexext));
+end
 solibs=unique(regexp(strsplit(solibs,'\n'),'/(\S+)','match','once'));
 solibs=solibs(cellfun(@(x) ~isempty(x),solibs));
 for ilib=1:length(solibs)
   fprintf('Copying %s to ../Libs/%s ...\n',solibs{ilib},mexext)
-  copyfile(solibs{ilib},sprintf('../Libs/%s',mexext),'f');
-  system(sprintf('svn add ../Libs/%s/%s',mexext,regexprep(solibs{ilib},'.*/','')));
+  try
+    copyfile(solibs{ilib},sprintf('../Libs/%s',mexext),'f');
+    system(sprintf('svn add ../Libs/%s/%s',mexext,regexprep(solibs{ilib},'.*/','')));
+  catch
+  end
 end
